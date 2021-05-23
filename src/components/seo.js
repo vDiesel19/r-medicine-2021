8fc291c062779888ab54713135d173870560a088
Vanessa Paolantonio <vpaolantonio@ripen.com>
2021-05-23 11:17:15 -0400

    abstract_form: trying to fix twitter card image URL

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import { StaticQuery, graphql } from "gatsby";
-import twitterCard from '../assets/r_medicine_twitter_card.jpg';
 
 const SEO = () => {
 	return (
@@ -14,6 +13,7 @@ const SEO = () => {
 							description
 							author
 							keywords
+							image
 							cardSummary
 							siteUrl
 						}
@@ -25,21 +25,21 @@ const SEO = () => {
 					{/* General tags */}
 					<title>{data.markdownRemark.frontmatter.title}</title>
 					<meta name="description" content={data.markdownRemark.frontmatter.description} />
-					<meta name="image" content={twitterCard} />
+					<meta name="image" content={data.markdownRemark.frontmatter.image} />
 					<link rel="canonical" href={data.markdownRemark.frontmatter.siteUrl} />
 
 					{/* OpenGraph tags */}
 					<meta property="og:url" content={data.markdownRemark.frontmatter.siteUrl} />
 					<meta property="og:title" content={data.markdownRemark.frontmatter.title} />
 					<meta property="og:description" content={data.markdownRemark.frontmatter.description} />
-					<meta property="og:image" content={twitterCard} />
+					<meta property="og:image" content={data.markdownRemark.frontmatter.image} />
 
 					{/* Twitter Card tags */}
 					<meta name="twitter:card" content="summary_large_image" />
 					<meta name="twitter:creator" content={data.markdownRemark.frontmatter.author} />
 					<meta name="twitter:title" content={data.markdownRemark.frontmatter.title} />
 					<meta name="twitter:description" content={data.markdownRemark.frontmatter.description} />
-					<meta name="twitter:image" content={twitterCard} />
+					<meta name="twitter:image" content={data.markdownRemark.frontmatter.image} />
         </Helmet>
 			)}
 		/>
--- src/markdown/seo.md
+++ src/markdown/seo.md
@@ -4,6 +4,7 @@ title: R/Medicine 2021 | Linux Foundation Events
 description: Join us in discussing the adoption of statistical modeling and reproducible data processing in clinical practice by promoting the use of the R programming environment and ecosystem.
 author: The Linux Foundation
 keywords: Registration and Abstract Submission Opening May 24th
+image: https://staging.r-medicine.org/static/r_medicine_twitter_card-45eb6b5168495dfaaddc8e62b26f181c.jpg
 cardSummary: Join us to learn about and discuss R based tools to improve clinical research and practice!
 siteUrl: https://r-medicine.org/
 ---
